58a2bec5639da371654a4e390813b51af30e9e1c
jean <jeanbrunemello@gmail.com>
2024-01-18 23:24:19 -0300

    criação do componente home, search e estilização

--- app-lista-compras/src/components/cards/listasCard/listasCard.js
+++ app-lista-compras/src/components/cards/listasCard/listasCard.js
@@ -1,47 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import css from './style.css'
-import BotaoDelete from '../../botoes/botaoDelete/botaoDelete';
-import BotaoEditar from '../../botoes/botaoEditar/botaoEditar';
+import React, { useEffect, useRef, useState } from 'react';
+import css from './style.css';
+import { useNavigate } from 'react-router-dom';
+import { TextField } from '@mui/material';
 import FormDialog from '../../modais/formDialog/formDialog';
-import { useNavigate} from 'react-router-dom';
 import listasController from '../../../controllers/listasController';
+import BotaoEditar from '../../botoes/botaoEditar/botaoEditar';
+import BotaoDelete from '../../botoes/botaoDelete/botaoDelete';
 
 function ListasCard() {
 
-    const [listas, setListas] = useState([])
+    const [listas, setListas] = useState([]);
+    const [listaOriginal, setListaOriginal] = useState([]);
+    const [pesquisa, setPesquisa] = useState('');
     const navigate = useNavigate();
+    const inputValueRef = useRef('');
 
     async function montarCards() {
         try {
             const listas = await listasController.buscarListas();
             setListas(listas);
+            setListaOriginal(listas);
         } catch (error) {
             console.error('Erro ao buscar listas:', error);
         }
     }
 
+    const aoInputChange = (event) => {
+        inputValueRef.current = event.target.value;
+        if (!inputValueRef.current) {
+            setListas(listaOriginal);
+        } else {
+            const listasFiltradas = listaOriginal.filter((lista) =>
+                lista.nome_lista.toLowerCase().includes(inputValueRef.current.toLowerCase())
+            );
+            setListas(listasFiltradas);
+        }
+        setPesquisa(inputValueRef.current);
+    };
     useEffect(() => {
         montarCards();
     }, []);
 
     return (
-        <div className='card-container'>
-            <FormDialog textoBtn={'add'} montarCards={montarCards}></FormDialog>
-            {listas.map((lista) => (
-                <div key={lista.id} className='card' >
-                    <div className="card-nome" onClick={() => navigate(`/listas/${lista.id}`, { state: { nome_lista: lista.nome_lista } })}>
-                        <p>{lista.nome_lista}</p>
-                    </div>
-                    <div className='botao-container'>
-                        <div className='botao-edit'>
-                            <BotaoEditar item={lista} montarCards={montarCards} ></BotaoEditar>
+        <div>
+            <div className='pesquisa-container'>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    id="nome_produto"
+                    name="nome_produto"
+                    label="Pesquisar..."
+                    type="text"
+                    fullWidth
+                    variant="standard"
+                    onChange={aoInputChange}
+                    InputProps={{
+                        style: {
+                            color: 'var(--secondary-color)',
+                        },
+                    }}
+                    InputLabelProps={{
+                        style: {
+                            color: 'var(--secondary-color)',
+                        },
+                    }}
+                />
+            </div>
+            <div className='card-container'>
+                <FormDialog textoBtn={'add'} montarCards={montarCards}></FormDialog>
+                {listas.map((lista) => (
+                    <div key={lista.id} className='card' >
+                        <div className="card-nome" onClick={() => navigate(`/listas/${lista.id}`, { state: { nome_lista: lista.nome_lista } })}>
+                            <p>{lista.nome_lista}</p>
                         </div>
-                        <div className='botao-delete'>
-                            <BotaoDelete listaId={lista.id} montarCards={montarCards}></BotaoDelete>
+                        <div className='botao-container'>
+                            <div className='botao-edit'>
+                                <BotaoEditar item={lista} montarCards={montarCards} ></BotaoEditar>
+                            </div>
+                            <div className='botao-delete'>
+                                <BotaoDelete listaId={lista.id} montarCards={montarCards}></BotaoDelete>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 }
--- app-lista-compras/src/pages/home/home.js
+++ app-lista-compras/src/pages/home/home.js
@@ -1,12 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import css from '../home/style.css';
-
-const conteudoTitulo = 'Home';
+import React, { useEffect, useRef, useState } from 'react';
+import css from './style.css';
+import { useNavigate } from 'react-router-dom';
+import listasController from '../../controllers/listasController';
+import { TextField } from '@mui/material';
+import BotaoEditar from '../../components/botoes/botaoEditar/botaoEditar';
+import BotaoDelete from '../../components/botoes/botaoDelete/botaoDelete';
+import Titulo from '../../components/titulo/titulo';
+import GroceryBagSvg from '../../assets/img/grocery_bag.svg';
 
 const Home = () => {
 
   return (
-    <p>{conteudoTitulo}</p>
+    <div>
+      <Titulo></Titulo>
+      <p>Seu app de listas de mercado</p>
+      <div className='imagem-flex'>
+        <img className='icone-compras' src={GroceryBagSvg} />
+      </div>
+    </div>
   );
 }
 
